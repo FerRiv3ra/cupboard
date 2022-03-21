08cfa31978c650d0d023dd61e4aefa2df369ef20
FerRivera <FerRivera2508@gmail.com>
2022-03-21 13:13:16 +0000

    Permision screen added

---
+++ src/components/Permission.js
@@ -0,0 +1,60 @@
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
+import React from 'react'
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faUnlockAlt } from '@fortawesome/free-solid-svg-icons'
+import { request, PERMISSIONS } from 'react-native-permissions';
+
+import globalStyles from '../styles/styles'
+
+const Permission = ({cameraStatus, checkPermission, setCameraStatus, setCameraPermission}) => {
+  const handlePermision = async () => {
+    const res = await request(Platform.OS === 'ios' ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA);
+    setCameraStatus(res);
+    if(res === 'granted'){
+      setCameraPermission(true);
+    }
+    checkPermission();
+  }
+
+  return (
+    <View>
+      <View style={styles.containerLogo}>
+        <Image
+            style={styles.logo}
+            source={require('../assets/logovc.png')}
+        />  
+      </View>
+      <Text
+        style={[globalStyles.label, globalStyles.textCenter]}
+      >This screen require camera permission to scan QR codes</Text>
+      {cameraStatus === 'denied' ? 
+      <Pressable 
+        style={[globalStyles.button, globalStyles.green]}
+        onPress={() => handlePermision()}
+      >
+        <FontAwesomeIcon 
+            style={[globalStyles.icon, {color: '#FFF'}]}
+            icon={faUnlockAlt}
+        />
+        <Text style={[globalStyles.textBtn, {color: '#FFF'}]}> grant permissions</Text>
+      </Pressable> :
+      <Text
+        style={[globalStyles.label, globalStyles.textCenter]}
+      >Please go to settings and allow permission to access to the camera</Text>}
+    </View>
+  )
+}
+
+export default Permission
+
+const styles = StyleSheet.create({
+    logo: {
+        width: 150,
+        height: 150,
+    },
+    containerLogo: {
+        alignSelf: 'center',
+        marginBottom: 20
+    },
+})
--- src/screens/NewDelivery.js
+++ src/screens/NewDelivery.js
@@ -1,12 +1,49 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Platform, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+import { check, PERMISSIONS } from 'react-native-permissions';
+import Permission from '../components/Permission';
 
 const NewDelivery = () => {
+  const [cameraStatus, setCameraStatus] = useState('');
+  const [cameraPermission, setCameraPermission] = useState(false);
+
+  useEffect(() => {
+    checkPermission();
+  }, []);
+
+  async function checkPermission(){
+    const res = await check(Platform.OS === 'ios' ? PERMISSIONS.IOS.CAMERA : PERMISSIONS.ANDROID.CAMERA);
+    setCameraStatus(res);
+    if(res === 'granted'){
+      setCameraPermission(true);
+    }
+  } 
+
   return (
-    <View>
-      <Text>NewDelivery</Text>
+    <View style={styles.container}>
+      <View style={{marginHorizontal: 30}}>
+        {!cameraPermission ? 
+           <Permission 
+            cameraStatus={cameraStatus}
+            checkPermission={checkPermission}
+            setCameraStatus={setCameraStatus}
+            setCameraPermission={setCameraPermission}
+           /> : 
+          <Text>Camera Screen</Text>
+        }
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  }
+})
 
 export default NewDelivery;
